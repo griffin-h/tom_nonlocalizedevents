27e43f569a63c35ea955c8a5970ba9bd55b0c912
Lindy Lindstrom <llindstrom@lco.global>
2021-02-12 18:31:51 +0000

    CandidateTarget model won't be subclass and makemigrations
    adds result of initial makemigrations (from host djangoproject)
    for EventLocalization model

--- tom_superevent/models.py
+++ tom_superevent/models.py
@@ -5,13 +5,6 @@ import tom_targets.models
 # Create your models here.
 
 
-class CandidateTarget(tom_targets.models.Target):
-    """This is an experiment to see if it makes sense to extend
-    the tom_targets.models.Target with a subclass.
-    """
-    pass
-
-
 class EventLocalization(models.Model):
     """Represents a region of the sky in which a superevent may have taken place.
     """
