8c50aa379c14c5dc27daa66bcf6a456c487de23d
Lindy Lindstrom <llindstrom@lco.global>
2021-02-12 19:57:16 +0000

    we need a model to represent a Superevent being followed-up upon

--- tom_superevent/models.py
+++ tom_superevent/models.py
@@ -5,6 +5,16 @@ import tom_targets.models
 # Create your models here.
 
 
+class Superevent(models.Model):
+    """Represents a Superevent being followed-up upon by this TOM.
+    For the moment, this is rather GraceDB specific, but sh/could be generalized to work
+    with gamma-ray bursts et al
+    """
+    # TODO: ask Curtis/Rachel/Andy about generalized use cases.
+    superevent_id = models.CharField(max_length=64)  # GraceDB superevent_id reference
+    superevent_url = models.URLField()
+
+
 class EventLocalization(models.Model):
     """Represents a region of the sky in which a superevent may have taken place.
     """
