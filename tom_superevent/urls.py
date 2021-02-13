e01c6d1b310372d0aeabc5d540ad722198a73a62
Lindy Lindstrom <llindstrom@lco.global>
2021-02-13 00:14:22 +0000

    remove hard-coded URL from template and provide app_name namespace

--- tom_superevent/urls.py
+++ tom_superevent/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+# app_name provides namespace in {% url %} template tag
+# (i.e. {% url 'superevents:detail' <pk> %}
+app_name = 'superevents'
+
 urlpatterns = [
     path('', views.SupereventListView.as_view(), name='index'),
     path('<int:pk>/', views.SupereventDetailView.as_view(), name='detail'),
