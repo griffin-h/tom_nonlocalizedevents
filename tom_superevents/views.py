2040af01632915b249e0d190d6da4fc5158eb0b4
Lindy Lindstrom <llindstrom@lco.global>
2021-02-17 17:49:23 +0000

    models need ViewSets

--- tom_superevents/views.py
+++ tom_superevents/views.py
@@ -1,7 +1,10 @@
 from django.views import generic
 
-from .models import Superevent
+from rest_framework import viewsets
+from rest_framework import permissions
 
+from .models import Superevent, EventLocalization
+from .serializers import SupereventSerializer, EventLocalizationSerializer
 
 class SupereventListView(generic.ListView):
     model = Superevent
@@ -11,3 +14,24 @@ class SupereventListView(generic.ListView):
 class SupereventDetailView(generic.DetailView):
     model = Superevent
     template_name = 'tom_superevents/detail.html'
+
+
+# Django Rest Framework Views
+
+
+class SupereventViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Superevents to be viewed or edited.
+    """
+    queryset = Superevent.objects.all()
+    serializer_class = SupereventSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class EventLocalizationViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows EventLocalizations to be viewed or edited.
+    """
+    queryset = EventLocalization.objects.all()
+    serializer_class = EventLocalizationSerializer
+    permission_classes = [permissions.IsAuthenticated]
