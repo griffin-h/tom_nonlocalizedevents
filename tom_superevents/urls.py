3ee07b45e06643b48bcefa14323840609dac710a
Lindy Lindstrom <llindstrom@lco.global>
2021-02-17 17:56:48 +0000

    add prelimanry router and URLConf
    there's an issue with the way this integrates with tom_base:
    tom_base defines an 'api/' endpoint for the REST API it knows
    about. as written this endpoint is 'superevents/api/' and should
    be better integrated with tom_base (if possible).

--- tom_superevents/urls.py
+++ tom_superevents/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from . import views
+
+router = routers.DefaultRouter()
+router.register(r'superevents', views.SupereventViewSet)
+router.register(r'eventlocalizations', views.EventLocalizationViewSet)
 
 # app_name provides namespace in {% url %} template tag
 # (i.e. {% url 'superevents:detail' <pk> %}
@@ -9,4 +14,7 @@ app_name = 'superevents'
 urlpatterns = [
     path('', views.SupereventListView.as_view(), name='index'),
     path('<int:pk>/', views.SupereventDetailView.as_view(), name='detail'),
+    # django restframework URLConf
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
