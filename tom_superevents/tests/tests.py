338323e18ab0e8cca72263b3a4ad1874b0e8ff94
Lindy Lindstrom <llindstrom@lco.global>
2021-02-18 01:49:26 +0000

    first trivial test to make sure testing infrastructure is complete
    it seems to be as the test is passing without errors

--- tom_superevents/tests/tests.py
+++ tom_superevents/tests/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from rest_framework.test import APITestCase
+
+from tom_superevents.tests.factories import SupereventFactory, EventLocalizationFactory
+
+
+class SupereventAPITestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='test_user')
+        self.superevent1 = SupereventFactory.create(superevent_id='superevent1')
+        self.superevent2 = SupereventFactory.create(superevent_id='superevent2')
+        self.eventlocalization1 = EventLocalizationFactory.create()
+        self.eventlocalization2 = EventLocalizationFactory.create()
+
+        # TODO: sort out django-guardian permissions
+        # assign_perm('tom_targets.view_target', self.user, self.st2)
+
+        self.client.force_login(self.user)
+
+
+class TestSupereventViewSet(SupereventAPITestCase):
+    def test_01(self):
+        pass
+
+
+class TestEventLocalizationViewSet(SupereventAPITestCase):
+    pass
