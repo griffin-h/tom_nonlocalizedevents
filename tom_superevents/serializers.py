34ccee314990c840fce8b707a7270e5cea17d5b2
Lindy Lindstrom <llindstrom@lco.global>
2021-02-25 00:41:24 +0000

    add housekeeping fields to serializers

--- tom_superevents/serializers.py
+++ tom_superevents/serializers.py
@@ -6,10 +6,12 @@ from rest_framework import serializers
 class SupereventSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Superevent
-        fields = ['superevent_id', 'superevent_url']
+        fields = ['superevent_id', 'superevent_url',
+                  'id', 'created', 'modified']
 
 
 class EventLocalizationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = EventLocalization
-        fields = []
+        fields = [
+            'id', 'created', 'modified']
