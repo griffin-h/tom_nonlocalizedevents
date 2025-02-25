f06ee19d17c31a6205bb8eac2e5fe898ebe6e484
William (Lindy) Lindstrom <llindstrom@lco.global>
2025-02-25 19:58:57 +0000

    add link to NLE index page to navbar
    nav_items is called by the inclusion tag tom_base/tom_common/templatetags/
    tom_common_extras.py::navbar_app_addons which inserts the specified
    partial into the navbar

--- tom_nonlocalizedevents/apps.py
+++ tom_nonlocalizedevents/apps.py
@@ -7,3 +7,13 @@ class NonLocalizedEventTypesConfig(AppConfig):
     def ready(self):
         import tom_nonlocalizedevents.signals.handlers  # noqa
         super().ready()
+
+    def nav_items(self):
+        """
+        Integration point for adding items to the navbar.
+        This method should return a list of dictionaries that include a `partial` key pointing to the html templates to
+        be included in the navbar. The `position` key, if included, should be either "left" or "right" to specify which
+        side of the navbar the partial should be included on. If not included, a right side nav item is assumed.
+        """
+        # add a single navbar item (defined in the partial) goto the NLE index page
+        return [{'partial': f'{self.name}/partials/navbar_nonlocalizedevents.html', 'position': 'left'}]
